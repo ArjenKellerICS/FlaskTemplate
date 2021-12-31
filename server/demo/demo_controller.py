5ac9bc27bbaf98f328e5b3f864fcad41473cec66
Arjen Keller <A.Keller@ams.icsvertex.nl>
2021-12-31 13:57:33 +0100

    initial commit

--- server/demo/demo_controller.py
+++ server/demo/demo_controller.py
@@ -1,13 +1,13 @@
 # imports
-from flask import request, jsonify, make_response
+from flask import request, jsonify, Response
 from flask.views import MethodView
 # in-module imports
 from .demo_interface import DemoInterface
 from .demo_service import DemoService
 from .demo_schema import DemoSchema
 
-demo_schema_many = DemoSchema(many=True)
-demo_schema_single = DemoSchema()
+schema_many = DemoSchema(many=True)
+schema_single = DemoSchema()
 
 
 class DemoMethodView(MethodView):
@@ -17,13 +17,10 @@ class DemoMethodView(MethodView):
 
     # 'GET' method
     def get(self):
-        return jsonify(demo_schema_many.dump(DemoService.get_all())), 200
+        return jsonify(schema_many.dump(DemoService.get_all())), 200
 
     def post(self):
-        new_data = DemoInterface(value=request.json["value"])
-        new = DemoService.create(new_data)
-
-        return jsonify(demo_schema_single.dump(new)), 200
+        return jsonify(schema_single.dump(DemoService.create(DemoInterface(value=request.json["value"])))), 200
 
 
 class DemoSingleMethodView(MethodView):
@@ -31,10 +28,13 @@ class DemoSingleMethodView(MethodView):
     This view can be used for multiple http methods
     """
     def get(self, demo_id: int):
-        return jsonify(demo_schema_single.dumps(DemoService.get_by_id(demo_id))), 200
+        return jsonify(schema_single.dumps(DemoService.get_by_id(demo_id))), 200
     
     def delete(self, demo_id: int):
         return jsonify(DemoService.delete_by_id(demo_id)), 200
+
+    def put(self, demo_id: int):
+        return jsonify(schema_single.dump(DemoService.update(DemoService.get_by_id(demo_id), DemoInterface(value=request.json["value"]))))
         
         
 
